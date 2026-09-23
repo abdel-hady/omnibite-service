0cc83cca271c13bb277141885e9ff756b939090e
abdel-hady <iomicrosoft505@gmail.com>
2026-09-23 10:37:41 +0800

    feat: add orders resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { UsersModule } from './users/users.module.js';
 import { JwtAuthGuard } from './auth/guards/auth.guard.js';
 import { RestaurantsModule } from './restaurants/restaurants.module.js';
 import { PrismaModule } from './prisma/prisma.module.js';
+import { OrdersModule } from './orders/orders.module.js';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { PrismaModule } from './prisma/prisma.module.js';
     AuthModule,
     UsersModule,
     RestaurantsModule,
+    OrdersModule,
   ],
   controllers: [AppController],
   providers: [
